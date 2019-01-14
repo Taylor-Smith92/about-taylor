35ab34f029c2a8b7ee12bb6d3ead8f62f3a86fbf
Taylor Smith <taylorleesmith92@gmail.com>
2019-01-14 14:59:46 -0700

    added basic framework, image is too large, looking for solution

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>About Taylor</title>
+	</head>
+
+	<body>
+		<p>
+			My name is Taylor Smith.
+		</p>
+		<p>
+			I play several instruments including guitar and drums.
+		</p>
+		<p>
+			My favorite color is purple.
+		</p>
+		<p>
+			I like Netflix.
+		</p>
+		<img src="IMG_3251.jpg" alt="This is a picture of Taylor">
+
+	</body>
+</html>
